a9a08dc2bc8e13e60b0d043ee7581c5f5a8332b3
Hossein Kord <h.kord@mcslab.local>
2019-10-06 12:45:25 +0330

    first commit

--- app/src/main/java/com/hosseinkurd/mvvmapp/network/Presenter.kt
+++
@@ -1,9 +0,0 @@
-package com.hosseinkurd.mvvmapp.network
-
-interface Presenter<V> {
-
-    fun onCallbackAttached(callback: V)
-
-    fun onCallbackDetached()
-
-}
--- app/src/main/java/com/hosseinkurd/mvvmapp/network/UserCallback.kt
+++
@@ -1,8 +0,0 @@
-package com.hosseinkurd.mvvmapp.network
-
-import com.hosseinkurd.loginsample.database.UserData
-import retrofit2.Callback
-
-interface UserCallback : Callback<UserData> {
-
-}
